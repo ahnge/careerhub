a81ad8f28c8c5bf0b8889b318ca65a66a47a1d11
ahnge <ahnge226@gmail.com>
2023-03-09 10:15:51 +0630

    raw database schema

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function jobSeeker()
+    {
+        return $this->hasOne(JobSeeker::class);
+    }
+
+    public function employer()
+    {
+        return $this->hasOne(Employer::class);
+    }
 }
